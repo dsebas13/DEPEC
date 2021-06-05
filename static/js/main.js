264f538e32b47434920b0800e47fca1f12c388ca
Daniel <dsebas13@gmail.com>
2021-06-05 10:32:46 -0300

    Busquedas-Postulacion

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,31 @@ if(btnAdd) {
   const btnArray = Array.from(btnAdd);
   btnArray.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-      if(!confirm('Esta seguro que desea postularse a esta busqueda?')){
+      if(!confirm('Esta seguro que desea postularse a esta búsqueda?')){
+        e.preventDefault();
+      }
+    });
+  })
+}
+
+const btnlock= document.querySelectorAll('.btn-lock');
+if(btnlock) {
+  const btnArray = Array.from(btnlock);
+  btnArray.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      if(!confirm('Esta seguro que desea cerrar esta busqueda, este proceso es irreversible?')){
+        e.preventDefault();
+      }
+    });
+  })
+}
+
+const btnpostu= document.querySelectorAll('.btn-postu');
+if(btnpostu) {
+  const btnArray = Array.from(btnpostu);
+  btnArray.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      if(!confirm('Esta seguro que desea inactivar su postulación?')){
         e.preventDefault();
       }
     });
